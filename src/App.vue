678200d228eb1175abf964646935a3f4295e8002
xingyifan <xingyifan10@qq.com>
2021-12-04 15:55:14 +0800

    feat:storage封装

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@
 </template>
 
 <script>
-  export default {
-    name: 'App'
-  }
+export default {
+  name: 'App',
+  mounted() {
+    this.$storage.setItem('userInfo', { age: 30, name: 'jack' });
+    console.log('user=>', this.$storage.getItem('userInfo'));
+    this.$storage.clearAll();
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
